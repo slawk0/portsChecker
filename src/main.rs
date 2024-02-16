807e5a7d01f55771995b6a6cf569a82a67b3302e
justgetahouse4 <86110674+justgetahouse4@users.noreply.github.com>
2024-02-16 23:36:28 +0100

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use std::io;
 use std::net::{TcpStream};
-
+use colored::Colorize;
 
 fn main() {
     println!("Welcome to the port scanner! (made by not you)");
@@ -12,20 +12,20 @@ fn main() {
     println!("Enter the ports to check, separated by commas (e.g. 80,443,8080):");
     let mut ports = String::new();
     io::stdin().read_line(&mut ports).expect("Failed to read ports");
-    println!("If not responding in 5 seconds the port is propably closed, you don't have to wait for it to finish ;)");
+    println!("If not responding in about 10 seconds the port is propably closed, you don't have to wait for it to finish ;)");
     let ports: Vec<_> = ports.trim().split(',').map(|p| p.trim().parse::<u16>().unwrap()).collect();
 
     for port in ports {
         match TcpStream::connect(format!("{}:{}", ip, port)) {
-            Ok(_) => println!("Port {} is open", port),
-            Err(_) => println!("Port {} is closed", port),
+            Ok(_) => println!("{} {} {}", "Port".green(), port, "is open".green()),
+            Err(_) => println!("{} {} {}", "Port".green(), port, "is closed".red()),
         }
     }
 
-    println!("Want do check another IP? (y/n)");
+    println!("Want do check another IP? Click ENTER if no type N");
     let mut answer = String::new();
     io::stdin().read_line(&mut answer).expect("Failed to read answer");
-    if answer.trim() == "y" {
+    if answer.trim() == "y" || answer.trim() == "Y" || answer.trim().is_empty() {
         main();
     } else {
         std::process::exit(0);
